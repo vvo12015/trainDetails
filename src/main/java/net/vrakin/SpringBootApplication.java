b65b545f45d4965a784d94cbcab99198616de7e4
vvo12015 <vvo12015@gmail.com>
2018-07-23 12:50:57 +0300

    -

--- src/main/java/net/vrakin/SpringBootApplication.java
+++ src/main/java/net/vrakin/SpringBootApplication.java
@@ -1,10 +1,11 @@
 package net.vrakin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@org.springframework.boot.autoconfigure.SpringBootApplication
 @EnableJpaAuditing
+@EnableAutoConfiguration
 public class SpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication.class, args);
--- src/main/resources/templates/login.ftl
+++ src/main/resources/templates/login.ftl
@@ -1,7 +1,7 @@
 <#import "parts/common.ftl" as c>
 <#import "parts/login.ftl" as l>
 
-<@c.page "Login page">
+<@c.page "My login page">
 Login page
 <@l.login "/login" />
 <a href="/registration">Registration</a>
