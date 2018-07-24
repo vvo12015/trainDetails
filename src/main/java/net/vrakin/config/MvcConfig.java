e4aee3facac921b23aada87aef9aa0f5574621d9
vvo12015 <vvo12015@gmail.com>
2018-07-24 12:56:17 +0300

    fix all bags related to the transition

--- src/main/java/net/vrakin/config/MvcConfig.java
+++ src/main/java/net/vrakin/config/MvcConfig.java
@@ -1,14 +1,4 @@
 package net.vrakin.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-    }
-
+public class MvcConfig {
 }
