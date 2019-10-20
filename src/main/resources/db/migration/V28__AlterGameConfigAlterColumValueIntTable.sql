4bd577a245108dcf317643e594125fba0e733f33
vvo12015 <vvo12015@gmail.com>
2019-10-21 00:53:55 +0300

    fix value_int from bigint on int

---
+++ src/main/resources/db/migration/V28__AlterGameConfigAlterColumValueIntTable.sql
@@ -0,0 +1,8 @@
+DROP table public.game_config;
+
+CREATE TABLE public.game_config(
+    id bigint NOT NULL PRIMARY KEY,
+    name character varying(255),
+    value_str character varying(255),
+    value_int int
+);
