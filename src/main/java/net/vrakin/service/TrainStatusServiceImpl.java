d82259dde7a69f7c3ff5275d9ac626d02617c325
unknown <vvo12015@gmail.com>
2019-10-25 15:01:28 +0300

    Added trainStatus model, repo, service and serviceimpl_01

--- src/main/java/net/vrakin/service/TrainStatusServiceImpl.java
+++ src/main/java/net/vrakin/service/TrainStatusServiceImpl.java
@@ -3,7 +3,9 @@ package net.vrakin.service;
 import net.vrakin.model.TrainStatus;
 import net.vrakin.repository.TrainStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TrainStatusServiceImpl extends GeneralAbstractService<TrainStatus> implements  TrainStatusService {
 
     @Autowired
