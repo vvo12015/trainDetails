b11387d445ad97c48976f9ec6fd9701c0d69ea84
unknown <vvo12015@gmail.com>
2021-03-27 23:13:24 +0200

    AlterDetailMuseumTable renamed columns reliability, max_power, added max_speed, start_date_production, price and created fields in model DetailMuseum each separately

--- src/main/resources/db/migration/V4__AlterDetailMuseumDelMaxPowerAddColumnsRenameColumnReliability .sql
+++ src/main/resources/db/migration/V4__AlterDetailMuseumDelMaxPowerAddColumnsRenameColumnReliability .sql
@@ -1,6 +1,10 @@
 ALTER TABLE ONLY public.detail_museum
-    RENAME COLUMN max_power TO max_mass,
-    RENAME COLUMN reliability TO start_reliability,
-    ADD COLUMN max_speed int,
-    ADD COLUMN start_date_production date,
+    RENAME COLUMN max_power TO max_mass;
+ALTER TABLE ONLY public.detail_museum
+    RENAME COLUMN reliability TO start_reliability;
+ALTER TABLE ONLY public.detail_museum
+    ADD COLUMN max_speed int;
+ALTER TABLE ONLY public.detail_museum
+    ADD COLUMN start_date_production date;
+ALTER TABLE ONLY public.detail_museum
     ADD COLUMN price int;
