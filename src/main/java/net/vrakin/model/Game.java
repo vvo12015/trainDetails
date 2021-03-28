b53e7a3d94f70ff70d1b44a2d8469fb03ed753f4
unknown <vvo12015@gmail.com>
2021-03-28 15:44:19 +0300

    Created table Game and GameUser, added releationship between they

---
+++ src/main/java/net/vrakin/model/Game.java
@@ -0,0 +1,54 @@
+package net.vrakin.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Table(name="game")
+@NoArgsConstructor
+@Data
+public class Game{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Exclude private Long id;
+
+    @Column(name = "game_time")
+    private Long gameTime;
+
+    @ManyToOne(fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "game_level_ref")
+    private GameLevel gameLevel;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JoinTable(name = "game_user",
+            joinColumns = @JoinColumn(name = "game_ref"),
+            inverseJoinColumns = @JoinColumn(name = "user_ref")
+    )
+    private List<User> users;
+
+    @ManyToOne(fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "main_user_ref")
+    private User mainUser;
+
+    public static List<String> getFields() {
+
+        List<String> fields = new ArrayList<>();
+
+        fields.add("id");
+        fields.add("name");
+
+        return fields;
+    }
+}
---
+++ src/main/resources/db/migration/V10__CreateTableGame.sql
@@ -0,0 +1,26 @@
+CREATE TABLE public.game(
+    id  bigint NOT NULL,
+    game_level_ref bigint,
+    game_time date,
+    main_user_ref bigint;
+);
+
+ALTER TABLE ONLY public.game
+    ADD CONSTRAINT game_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.game
+    ADD CONSTRAINT game_game_level FOREIGN KEY (game_level_ref) REFERENCES public.game_level(id);
+
+ALTER TABLE ONLY public.game
+    ADD CONSTRAINT game_user FOREIGN KEY (main_user_ref) REFERENCES public.users(id);
+
+CREATE TABLE public.game_user(
+    game_ref bigint,
+    user_ref bigint;
+);
+
+ALTER TABLE ONLY public.game_user
+    ADD CONSTRAINT user_game_user FOREIGN KEY (user_ref) REFERENCES public.users(id);
+
+ALTER TABLE ONLY public.game_user
+    ADD CONSTRAINT game_game_user FOREIGN KEY (game_ref) REFERENCES public.game(id);
