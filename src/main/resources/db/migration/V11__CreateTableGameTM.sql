43a8de426efbbce7e7486bed82f738d8324a6e90
unknown <vvo12015@gmail.com>
2021-04-10 00:08:12 +0300

    added gametm, game_state tables and filled data game_level and game_state

---
+++ src/main/resources/db/migration/V11__CreateTableGameTM.sql
@@ -0,0 +1,32 @@
+DROP TABLE gameTM;
+CREATE TABLE public.gameTM(
+    id  bigint NOT NULL,
+    map_ref bigint,
+    game_level_ref bigint,
+    current_gamer_ref bigint,
+    game_state_ref bigint
+    profile json
+);
+
+ALTER TABLE ONLY public.gameTM
+    ADD CONSTRAINT gametm_pkey PRIMARY KEY (id);
+
+CREATE TABLE public.game_state(
+    id  bigint NOT NULL,
+    name character varying(255)
+);
+
+ALTER TABLE ONLY public.gameTM
+    ADD CONSTRAINT gametm_game_level FOREIGN KEY (game_level_ref) REFERENCES public.game_level(id);
+
+ALTER TABLE ONLY public.gameTM
+    ADD CONSTRAINT gametm_game_state FOREIGN KEY (game_state_ref) REFERENCES public.game_state(id);
+
+INSERT TABLE game_level(name)
+    values("beginner"),
+    ("middle"),
+    ("hard");
+
+INSERT TABLE game_state(name)
+    values("begin"),
+    ("end");
